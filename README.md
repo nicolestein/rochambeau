81c32c63694526ded7d5b5818325b5330d26137d
Giorvi Alvarez <giorvialvarez@Giorvis-MacBook-Pro.local>
2018-06-02 08:33:09 -0700

    added README.md file. Match Score is done!!

---
+++ README.md
@@ -0,0 +1,27 @@
+Description
+
+   ** User Story 1 **
+    
+    As a player, I want to be able to choose rock, paper, scissors, Spock or lizard and then compete against the computer who also chooses rock, paper, scissors, Spock or lizard. I want to be able to know who won the game based on the ruleset of the game (see below).
+   
+   ** RuleSet ** 
+   
+    There are 5 valid moves: Rock, Paper, Scissors, Spock, Lizard
+    Rock breaks Scissors or kills Lizard (Rock wins)
+    Paper covers Rock and beats Spock (Paper wins)
+    Scissors cut Paper and kills Lizard (Scissors wins)
+    Spock defeats Scissors or Rock (Spock wins)
+    Lizard defeats Spock and Paper (Lizard wins)
+    
+   ** User Story 2 **
+    
+    As a user, I want to see a scoreboard that tells me how many matches I have own and how many the computer has won. A match is defined by a "best out of three games". 
+    
+    Based on this user story the app was made so a person can play againest the computer that can randomly choose between a set number of choices Rock, Paper, Scissors, Lizard or Spock. the user can make a choose what they want to play and then when they have made their choice can press a button initiating the computuers turn. 
+    
+    There are two score boards on is the current score which includes wins losses and ties. The other score board is a match score board that takes the best of three games and tells the user how many times they have one a match or the computer has won a match. 
+    
+
+A link to the Trello baord.
+
+    https://github.com/nicolestein/rochambeau.git
--- rochambeau.js
+++ rochambeau.js
@@ -8,8 +8,11 @@ var choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
 
 // Variable to store the score
 // score[0] = wins, score[1] = ties, score[2] = losses
-var score = [0,0,0];
+var score = [0, 0, 0];
 
+// Stores match score that will be used for the match score
+// Matchscore[0] = match wins, matchScore[1] = match losses
+var matchScore = [0, 0];
 // Stores the player's choice, then call's the function for storing the computer's choice
 function storePlayerChoice(choice) {
     playerChoice = choice;
@@ -19,66 +22,78 @@ function storePlayerChoice(choice) {
 
 // Generate computer's random choice
 function storeComputerChoice() {
-    computerChoice = Math.floor(Math.random()*5);
+    computerChoice = Math.floor(Math.random() * 5);
     console.log("Computer choice = " + computerChoice);
 }
 
 // This is the function for playing the game
-function playGame(){
+function playGame() {
     // Here is the game ruleset algorithm
     if (playerChoice == computerChoice) {
         // We have a tie!
         updateScore(1);
-        displayGameResult("tie")
+        displayGameResult("tie");
+        updateMatchScore();
     } else if (playerChoice == 0 && computerChoice == 2) {
         // Rock beats scissors - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 0 && computerChoice == 3) {
         // Rock beats lizard - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 1 && computerChoice == 0) {
         // Paper beats rock - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 1 && computerChoice == 4) {
         // Paper beats spock - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 2 && computerChoice == 1) {
         // Scissors beats paper - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 2 && computerChoice == 3) {
         // Scissors beats lizard - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 4 && computerChoice == 2) {
         // Spock beats scissors - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 4 && computerChoice == 0) {
         // Spock beats rock - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 3 && computerChoice == 4) {
         // Lizard beats spock - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else if (playerChoice == 3 && computerChoice == 1) {
         // Lizard beats paper - a win!
         updateScore(0);
-        displayGameResult("win")
+        displayGameResult("win");
+        updateMatchScore();
     } else {
         // All other combinations are losses
         updateScore(2);
-        displayGameResult("lose")
+        displayGameResult("lose");
+        updateMatchScore();
     }
 }
 
 //Displays the result of the game
-function displayGameResult(result){
+function displayGameResult(result) {
     // Define an array of text labels for the choices 0, 1, 2;
     // Create a message for the player
     var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
@@ -98,19 +113,39 @@ function displayGameResult(result){
     }
 
     updateScoreBoard();
+    updateMatchScoreBoard();
 }
 
 // Updates the score
-function updateScore(val){
+function updateScore(val) {
     ++score[val];
     console.log("The score is now " + score);
 }
 
+function updateMatchScore() {
+    if (score[0] == 2) {
+        matchScore[0]++;
+        score[0] = 0;
+        score[1] = 0;
+        score[2] = 0;
+    } else if (score[2] == 2) {
+        matchScore[1]++;
+        score[0] = 0;
+        score[1] = 0;
+        score[2] = 0;
+    }
+}
+
 // Function for displaying the score
-function updateScoreBoard(){
+function updateScoreBoard() {
     document.getElementById("wins").textContent = score[0];
     document.getElementById("losses").textContent = score[2];
     document.getElementById("ties").textContent = score[1];
+}
+
+function updateMatchScoreBoard() {
+    document.getElementById("Match Wins").textContent = matchScore[0];
+    document.getElementById("Match Losses").textContent = matchScore[1];
 }
 
 // The button elements
@@ -122,9 +157,21 @@ var spockButton = document.getElementById("spock");
 var playButton = document.getElementById("play");
 
 // Add the event handlers
-rockButton.addEventListener('click', () => {storePlayerChoice(0)});
-paperButton.addEventListener('click', () => {storePlayerChoice(1)});
-scissorsButton.addEventListener('click', () => {storePlayerChoice(2)});
-lizardButton.addEventListener('click', () => {storePlayerChoice(3)});
-spockButton.addEventListener('click', () => {storePlayerChoice(4)});
-playButton.addEventListener('click', () => {playGame()});
+rockButton.addEventListener('click', () => {
+    storePlayerChoice(0)
+});
+paperButton.addEventListener('click', () => {
+    storePlayerChoice(1)
+});
+scissorsButton.addEventListener('click', () => {
+    storePlayerChoice(2)
+});
+lizardButton.addEventListener('click', () => {
+    storePlayerChoice(3)
+});
+spockButton.addEventListener('click', () => {
+    storePlayerChoice(4)
+});
+playButton.addEventListener('click', () => {
+    playGame()
+});
